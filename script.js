b838366f78c6e694caaad3e9582f9304b9491bbd
SmokeyZZZ <pierounico30@gmail.com>
2024-04-01 16:20:13 +0200

    implemented the basic behaviours of the todolist

---
+++ script.js
@@ -0,0 +1,46 @@
+
+const todos = document.querySelector("#todos");
+const addBtn = document.querySelector("#add");
+const text = document.querySelector("#text")
+const listOfTodo = [];
+let todo ;
+
+function addingATodo()
+{
+    todo= document.createElement("div");
+    todo.classList.add("card")
+    todo.id = "todo";
+    todo.innerHTML = '<h2 id="textOfTodo">'+text.value+'</h2><button class="Btn" id = "remove">X</button>'
+    text.value ="";
+    todos.appendChild(todo);
+}
+addBtn.addEventListener("click",addingATodo);
+document.addEventListener('keydown', function(event) {
+    if(event.key == "Enter") 
+    {
+        if(text.value!="")
+        {
+            addingATodo();
+        }
+    }
+    
+});
+todos.addEventListener("click",function(e){
+    if(e.target.id =="remove")
+    {
+        todos.removeChild(e.target.parentElement);
+    }
+    else if(e.target.id=="textOfTodo")
+    {
+        if(e.target.style.textDecoration=="none")
+        {
+            e.target.style.textDecoration ="line-through";
+        }
+        else
+        {
+            e.target.style.textDecoration ="none";
+
+        }
+    }
+    
+});
